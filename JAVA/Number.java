ae2dc3fd1397b80e62c9a232273b059a6314897d
Park JuHo <86903267+Lokiest@users.noreply.github.com>
2022-03-07 11:56:14 +0900

    Create Number.java

---
+++ JAVA/Number.java
@@ -0,0 +1,17 @@
+
+public class Number {
+
+	public static void main(String[] args) {
+		// Operator
+		System.out.println(10 + 2); // 12
+		System.out.println(10 - 2); // 8
+		System.out.println(10 * 2); // 20
+		System.out.println(10 / 2); // 5
+		
+		System.out.println(Math.sqrt(4)); // 2
+		System.out.println(Math.PI); // PI
+		System.out.println(Math.floor(Math.PI)); // 3 (내림)
+		System.out.println(Math.ceil(Math.PI)); // 4 (올림)
+	}
+
+}
