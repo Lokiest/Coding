87b49fe402aa8c455118f601bfe68fb586137cf1
Park JuHo <86903267+Lokiest@users.noreply.github.com>
2022-03-07 11:56:37 +0900

    Create StringApp.java

---
+++ JAVA/StringApp.java
@@ -0,0 +1,18 @@
+
+public class StringApp {
+
+	public static void main(String[] args) {
+		System.out.println("PPakDoRyeon");
+		System.out.println('P'); // 한 글자를 표현
+		// 자바에서 작은 따옴표는 Charater를 나타냄
+		// Charater의 집합이 String
+		System.out.println("P"); // String
+		System.out.println("PPak"
+				+ "DoRyeon"); // 엔터만 치면 알아서 나눠줌
+		System.out.println("PPak\nDoRyeon"); // 줄바꿈
+		// escape
+		System.out.println("PPak\"DoRyeon\""); // PPak"DoRyeon"
+
+	}
+
+}
