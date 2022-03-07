adf00988f48751639c30ffc431997b08550718f7
Park JuHo <86903267+Lokiest@users.noreply.github.com>
2022-03-07 11:56:52 +0900

    Create String_operations.java

---
+++ JAVA/String_operations.java
@@ -0,0 +1,11 @@
+
+public class StringOperation {
+
+	public static void main(String[] args) {
+		System.out.println("PPakDoRyeon".length()); // 글자 수 세기
+		System.out.println("[[[name]]]".replace("[[[name]]]"
+				, "PPakDoRyeon")); // replace
+
+	}
+
+}
