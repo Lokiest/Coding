750a06b0e047196a522dd3e829b0e8a1342bb5c9
Park JuHo <86903267+Lokiest@users.noreply.github.com>
2022-03-07 11:55:53 +0900

    Create DataType.java

---
+++ JAVA/DataType.java
@@ -0,0 +1,17 @@
+public class datatype{
+	public static void main(String[] args) {
+		System.out.println(2); // number
+		System.out.println("two"); // String
+		
+		System.out.println("2"); // String 2
+		System.out.println(2 + 2); // 4
+		System.out.println("2" + "2"); // 22
+		
+		System.out.println(2 * 2); // 4
+		//System.out.println("2" * "2"); 불가능
+		System.out.println("2222".length()); // 4 (문자열 길이)
+		//System.out.println(2222.length()); // 불가능
+		
+		
+	}
+}
