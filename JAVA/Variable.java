c3706edeca0ed49061d9df36451807fd8b811cd9
Park JuHo <86903267+Lokiest@users.noreply.github.com>
2022-03-08 21:38:31 +0900

    Create Variable.java

---
+++ JAVA/Variable.java
@@ -0,0 +1,16 @@
+
+public class Variable {
+
+	public static void main(String[] args) {
+		int a = 1; // 변수 만들시 데이터타입 설정 필요
+		System.out.println(a);
+		
+		double b = 1.1; // 실수
+		System.out.println(b);
+		
+		String c = "PPakDoRyeon";
+		System.out.println(c);
+
+	}
+
+}
