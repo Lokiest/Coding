02b3fa1e1ea742ad5d57a102828c391b9877b3e8
Park JuHo <86903267+Lokiest@users.noreply.github.com>
2022-03-10 22:34:53 +0900

    Create classApp.java

---
+++ JAVA/Programming/classApp.java
@@ -0,0 +1,12 @@
+
+public class ClassApp {
+
+	public static void main(String[] args) {
+		
+		System.out.println(Math.PI);
+		System.out.println(Math.floor(3.9));
+		System.out.println(Math.ceil(3.2));
+
+	}
+
+}
