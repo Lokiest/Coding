4ec06af8edd12897ddd8da00e0ec02e6cc3ac203
Park JuHo <86903267+Lokiest@users.noreply.github.com>
2022-03-10 22:35:15 +0900

    Create InstanceApp.java

---
+++ JAVA/Programming/InstanceApp.java
@@ -0,0 +1,17 @@
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+public class InstanceApp {
+
+	public static void main(String[] args) throws IOException {
+		PrintWriter p1 = new PrintWriter("result1.txt");
+		p1.write("Hi 1");
+		p1.close();
+		
+		PrintWriter p2 = new PrintWriter("result2.txt");
+		p2.write("Hi 2");
+		p2.close();
+
+	}
+
+}
