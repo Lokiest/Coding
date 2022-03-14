800ffd770ebcfc2a5246d844a511bc06678020bf
Park JuHo <86903267+Lokiest@users.noreply.github.com>
2022-03-14 12:23:33 +0900

    Create Boolean.java

---
+++ JAVA/java_flow_control/Boolean.java
@@ -0,0 +1,19 @@
+
+public class BooleanApp {
+
+	public static void main(String[] args) {
+		System.out.println("2");
+		System.out.println(2);
+		
+		System.out.println(true);
+		System.out.println(false);
+		
+		String Something = "123";
+		//String true = "123"; 변수의 이름으로 사용 불가능 (reserved word)
+		
+		System.out.println(Something.contains("12"));
+		System.out.println(Something.contains("45"));
+		
+	}
+
+}
