fc30f7a90d162e76fe1f8e75805fe19de269bf83
Park JuHo <86903267+Lokiest@users.noreply.github.com>
2022-03-16 14:00:27 +0900

    Create IfApp.java

---
+++ JAVA/java_flow_control/IfApp.java
@@ -0,0 +1,29 @@
+
+public class IfApp {
+
+	public static void main(String[] args) {
+		
+		System.out.println("a");
+		
+		//if (false) {
+		//	System.out.println(1);
+		//} else {
+		//	if (true) {
+		//		System.out.println(2);
+		//	}
+		//	System.out.println(3);
+		//}
+		//System.out.println("b");
+		
+		if (false) {
+			System.out.println(1);
+		} else if (true) {
+			System.out.println(2);
+		} else {
+			System.out.println(3);
+		}
+		System.out.println("b");
+
+	}
+
+}
