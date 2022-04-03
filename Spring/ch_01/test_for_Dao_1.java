083544b1d316d6613a0c6976c6e077b793006316
Park JuHo <86903267+Lokiest@users.noreply.github.com>
2022-04-03 14:01:36 +0900

    Create test_for_Dao_1.java

---
+++ Spring/ch_01/test_for_Dao_1.java
@@ -0,0 +1,28 @@
+package springbook.user.dao;
+
+import java.sql.SQLException;
+
+import springbook.user.domain.User;
+
+public class test {
+
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		UserDao dao = new UserDao();
+		
+		User user = new User();
+		user.setId("SM");
+		user.setName("AESPA");
+		user.setPassword("SAVAGE");
+		
+		dao.add(user);
+		
+		System.out.println(user.getId() + " 등록 성공");
+		
+		User user2 = dao.get(user.getId());
+		System.out.println(user2.getName());
+		System.out.println(user2.getPassword());
+		
+		System.out.println(user2.getId() + " 조회 성공");
+	}
+
+}
