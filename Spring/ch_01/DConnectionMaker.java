133c6d2f2a1d642222aeaf1dd71b26450c1c2390
Park JuHo <86903267+Lokiest@users.noreply.github.com>
2022-04-08 16:14:41 +0900

    Update DConnectionMaker.java

--- Spring/ch_01/DConnectionMaker.java
+++ Spring/ch_01/DConnectionMaker.java
@@ -1,12 +1,16 @@
 package springbook.user.dao;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class DConnectionMaker implements ConnectionMaker{
 	//...
 	public Connection makeConnection() throws ClassNotFoundException, SQLException {
-		return null;
+		Class.forName("oracle.jdbc.driver.OracleDriver");
+		Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521/xe", "system", "1001");
+		return con;
 		//D사의 독자적인 방법으로 Connection을 생성하는 코드
 	}
 }
