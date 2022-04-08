42ce1855a7169436e598bc0c40cff53278e2467f
Park JuHo <86903267+Lokiest@users.noreply.github.com>
2022-04-08 15:30:10 +0900

    Create DConnectionMaker.java

---
+++ Spring/ch_01/DConnectionMaker.java
@@ -0,0 +1,12 @@
+package springbook.user.dao;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class DConnectionMaker implements ConnectionMaker{
+	//...
+	public Connection makeConnection() throws ClassNotFoundException, SQLException {
+		return null;
+		//D사의 독자적인 방법으로 Connection을 생성하는 코드
+	}
+}
