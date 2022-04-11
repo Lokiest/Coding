70cbf85033d29c07a3aa6e34fe6670329a0f4174
Park JuHo <86903267+Lokiest@users.noreply.github.com>
2022-04-11 18:14:36 +0900

    Create DaoFactory_3.java

---
+++ Spring/ch_01/DaoFactory_3.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DaoFactory {
+	
+	@Bean
+	public UserDao userDao() {
+		return new UserDao(connectionMaker());
+	}
+	
+	@Bean
+	public ConnectionMaker connectionMaker() { 
+		return new DConnectionMaker();
+	}
+}
