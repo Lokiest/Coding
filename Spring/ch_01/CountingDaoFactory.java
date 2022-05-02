e00ef78d52b62086e41f1192a7571654b4da7627
Park JuHo <86903267+Lokiest@users.noreply.github.com>
2022-05-02 22:12:48 +0900

    Create CountingDaoFactory.java

---
+++ Spring/ch_01/CountingDaoFactory.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DaoFactory {
+	
+	@Bean
+	public UserDao userDao() {
+		return new UserDao(connectionMaker());
+	}
+	
+	@Bean
+	public ConnectionMaker connectionMaker() {
+		return new CountingConnectionMaker(realConnectionMaker());
+	}
+	
+	@Bean
+	public ConnectionMaker realConnectionMaker() { 
+		return new DConnectionMaker();
+	}
+}
