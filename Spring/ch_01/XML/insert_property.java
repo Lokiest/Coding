881d7e6daf38720e0bf841f63c869473b00f2edc
Park JuHo <86903267+Lokiest@users.noreply.github.com>
2022-05-07 13:28:19 +0900

    Create insert_property.java

---
+++ Spring/ch_01/XML/insert_property.java
@@ -0,0 +1,4 @@
+<property name = "driverClass" value = "com.mysql.jdbc.Driver" />
+<property name = "url" value = "jdbc:mysql://localhost/springbook" />
+<property name = "username" value = "~" />
+<property name = "password" value = "~" />
