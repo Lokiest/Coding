eeb191275077b0ceb17ffc28cc5331b082cbe12f
Park JuHo <86903267+Lokiest@users.noreply.github.com>
2022-05-10 15:54:50 +0900

    Create main_method_with_JUnit.java

---
+++ Spring/ch_02/main_method_with_JUnit.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+import org.junit.runner.JUnitCore;
+
+public class JTest {
+
+	public static void main(String[] args) {
+		JUnitCore.main("com.example.demo.UserDaoTest");
+
+	}
+
+}
