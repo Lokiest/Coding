ac95df4a4b91fd61f19515faeffaedd564d87e4b
Park JuHo <86903267+Lokiest@users.noreply.github.com>
2022-06-26 16:58:09 +0900

    Create RowMapper.java

---
+++ Spring/ch_03/3.6/RowMapper.java
@@ -0,0 +1,11 @@
+//재사용 가능하도록 독립시킨 RowMapper
+private RowMapper<User> userMapper = 
+		new RowMapper<User>() {
+				public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+				User user = new User();
+				user.setId(rs.getString("id"));
+				user.setName(rs.getString("name"));
+				user.setPassword(rs.getString("password"));
+				return user;
+			}
+		};
