db4245df0f0ca20c2f245bde57d9fd09df822e5c
Park JuHo <86903267+Lokiest@users.noreply.github.com>
2022-06-26 16:57:17 +0900

    Create Test_for_getAll().java

---
+++ Spring/ch_03/3.6/Test_for_getAll().java
@@ -0,0 +1,32 @@
+//getAll()에 대한 테스트
+@Test
+	public void getAll()  {
+		dao.deleteAll();
+		
+		List<User> users0 = dao.getAll();
+		assertThat(users0.size(), is(0));
+		
+		dao.add(user1); // Id: gyumee
+		List<User> users1 = dao.getAll();
+		assertThat(users1.size(), is(1));
+		checkSameUser(user1, users1.get(0));
+		
+		dao.add(user2); // Id: leegw700
+		List<User> users2 = dao.getAll();
+		assertThat(users2.size(), is(2));
+		checkSameUser(user1, users2.get(0));  
+		checkSameUser(user2, users2.get(1));
+		
+		dao.add(user3); // Id: bumjin
+		List<User> users3 = dao.getAll();
+		assertThat(users3.size(), is(3));
+		checkSameUser(user3, users3.get(0));  
+		checkSameUser(user1, users3.get(1));  
+		checkSameUser(user2, users3.get(2));  
+	}
+
+	private void checkSameUser(User user1, User user2) {
+		assertThat(user1.getId(), is(user2.getId()));
+		assertThat(user1.getName(), is(user2.getName()));
+		assertThat(user1.getPassword(), is(user2.getPassword()));
+	}
