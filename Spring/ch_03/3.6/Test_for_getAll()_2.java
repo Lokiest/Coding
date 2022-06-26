06d624c150c92e23494ea2261183624ad26b5fe6
Park JuHo <86903267+Lokiest@users.noreply.github.com>
2022-06-26 16:57:53 +0900

    Create Test_for_getAll()_2.java

---
+++ Spring/ch_03/3.6/Test_for_getAll()_2.java
@@ -0,0 +1,26 @@
+//데이터 없는 경우에 대한 검증 추가된 getAll() 테스트
+@Test
+	public void getAll()  {
+		dao.deleteAll();
+		
+		List<User> users0 = dao.getAll();
+		assertThat(users0.size(), is(0));
+		
+		dao.add(user1); // Id: gyumee
+		List<User> users1 = dao.getAll();
+		assertThat(users1.size(), is(1));
+		checkSameUser(user1, users1.get(0));
+		
+		dao.add(user2); // Id: leegw700
+		List<User> users2 = dao.getAll();
+		assertThat(users2.size(), is(2));
+		checkSameUser(user1, users2.get(0));  
+		checkSameUser(user2, users2.get(1));
+		
+		dao.add(user3); // Id: bumjin
+		List<User> users3 = dao.getAll();
+		assertThat(users3.size(), is(3));
+		checkSameUser(user3, users3.get(0));  
+		checkSameUser(user1, users3.get(1));  
+		checkSameUser(user2, users3.get(2));  
+	}
