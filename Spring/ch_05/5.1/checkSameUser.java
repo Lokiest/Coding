0167ab756db8fbd8dcccb9515ee1709a5c4723d6
Park JuHo <86903267+Lokiest@users.noreply.github.com>
2022-07-13 00:19:32 +0900

    Create checkSameUser.java

---
+++ Spring/ch_05/5.1/checkSameUser.java
@@ -0,0 +1,9 @@
+//새로운 필드를 포함하는 User 필드 값 검증 메소드
+private void checkSameUser(User user1, User user2) {
+		assertThat(user1.getId(), is(user2.getId()));
+		assertThat(user1.getName(), is(user2.getName()));
+		assertThat(user1.getPassword(), is(user2.getPassword()));
+		assertThat(user1.getLevel(), is(user2.getLevel()));
+		assertThat(user1.getLogin(), is(user2.getLogin()));
+		assertThat(user1.getRecommend(), is(user2.getRecommend()));
+	}
