23ead22360c7644ce03bd9b0a1551eef37d4e705
Park JuHo <86903267+Lokiest@users.noreply.github.com>
2022-07-13 00:19:00 +0900

    Create UserDao_2.java

---
+++ Spring/ch_05/5.1/UserDao_2.java
@@ -0,0 +1,69 @@
+//추가된 필드를 파라미터로 포함하는 생성자
+//User에 추가
+package springbook.user.domain;
+
+public class User {
+	String id;
+	String name;
+	String password;
+	Level level;
+	int login;
+	int recommend;
+	
+	public User() {
+	}
+	
+	public User(String id, String name, String password, Level level,
+			int login, int recommend) {
+		this.id = id;
+		this.name = name;
+		this.password = password;
+		this.level = level;
+		this.login = login;
+		this.recommend = recommend;
+	}
+
+
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Level getLevel() {
+		return level;
+	}
+
+	public void setLevel(Level level) {
+		this.level = level;
+	}
+
+	public int getLogin() {
+		return login;
+	}
+
+	public void setLogin(int login) {
+		this.login = login;
+	}
+
+	public int getRecommend() {
+		return recommend;
+	}
+
+	public void setRecommend(int recommend) {
+		this.recommend = recommend;
+	}
+}
