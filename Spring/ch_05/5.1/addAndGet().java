c1bc36c06a02843bfe9dc208bb031d8941b73e4e
Park JuHo <86903267+Lokiest@users.noreply.github.com>
2022-07-13 00:19:50 +0900

    Create addAndGet().java

---
+++ Spring/ch_05/5.1/addAndGet().java
@@ -0,0 +1,16 @@
+//checkSameUser() 메소드를 사용하도록 만든 addAndGet()
+@Test 
+	public void addAndGet() {		
+		dao.deleteAll();
+		assertThat(dao.getCount(), is(0));
+
+		dao.add(user1);
+		dao.add(user2);
+		assertThat(dao.getCount(), is(2));
+		
+		User userget1 = dao.get(user1.getId());
+		checkSameUser(userget1, user1);
+		
+		User userget2 = dao.get(user2.getId());
+		checkSameUser(userget2, user2);
+	}
