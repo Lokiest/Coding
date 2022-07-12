30092032ce91344e19137cb4f7159d44504bc5a5
Park JuHo <86903267+Lokiest@users.noreply.github.com>
2022-07-13 00:17:29 +0900

    Update Level_enum.java

--- Spring/ch_05/5.1/Level_enum.java
+++ Spring/ch_05/5.1/Level_enum.java
@@ -1,10 +1,24 @@
-//정수형 상수 값으로 정의한 레벨
-class User {
-    private static final int BASIC = 1;
-    private static final int SILVER = 2;
-    private static final int GOLD = 3;
+//레벨용 이늄
+package springbook.user.domain;
 
-    int level;
-    public void setLevel(int level) {
-        this.level = level;
-}
+public enum Level {
+	BASIC(1), SILVER(2), GOLD(3); //세 개의 이늄 오브젝트 정의
+
+	private final int value;
+		
+	Level(int value) { //DB에 저장할 값을 넣어줄 생성자 생성
+		this.value = value;
+	}
+
+	public int intValue() { //값 가져오기
+		return value;
+	}
+	
+	public static Level valueOf(int value) { //값으로부터 Level 타입 오브젝트를 가져오도록 만든 스태틱 메소드
+		switch(value) {
+		case 1: return BASIC;
+		case 2: return SILVER;
+		case 3: return GOLD;
+		default: throw new AssertionError("Unknown value: " + value);
+		}
+	}
