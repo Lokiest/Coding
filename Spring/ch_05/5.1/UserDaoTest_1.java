08bfecfcac3cc9389cc0309739ebbc5a047dcad6
Park JuHo <86903267+Lokiest@users.noreply.github.com>
2022-07-13 00:18:25 +0900

    Create UserDaoTest_1.java

---
+++ Spring/ch_05/5.1/UserDaoTest_1.java
@@ -0,0 +1,44 @@
+//수정된 테스트 픽스처
+package springbook.user.dao;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.support.SQLErrorCodeSQLExceptionTranslator;
+import org.springframework.jdbc.support.SQLExceptionTranslator;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import springbook.user.domain.Level;
+import springbook.user.domain.User;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations="/test-applicationContext.xml")
+public class UserDaoTest {
+	@Autowired UserDao dao; 
+	@Autowired DataSource dataSource;
+	
+	private User user1;
+	private User user2;
+	private User user3; 
+	
+	@Before
+	public void setUp() {
+		this.user1 = new User("gyumee", "박성철", "springno1", Level.BASIC, 1, 0);
+		this.user2 = new User("leegw700", "이길원", "springno2", Level.SILVER, 55, 10);
+		this.user3 = new User("bumjin", "박범진", "springno3", Level.GOLD, 100, 40);
+	}
+    ...
+}
