27a1160eeb713f3edfc0eb7f27f6b349a4315f54
Park JuHo <86903267+Lokiest@users.noreply.github.com>
2022-07-13 00:16:57 +0900

    Create Level_enum.java

---
+++ Spring/ch_05/5.1/Level_enum.java
@@ -0,0 +1,10 @@
+//정수형 상수 값으로 정의한 레벨
+class User {
+    private static final int BASIC = 1;
+    private static final int SILVER = 2;
+    private static final int GOLD = 3;
+
+    int level;
+    public void setLevel(int level) {
+        this.level = level;
+}
