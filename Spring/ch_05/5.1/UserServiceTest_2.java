bc5ace44a82e9314b1c21e1792e35eea091d19a7
Park JuHo <86903267+Lokiest@users.noreply.github.com>
2022-07-31 21:47:52 +0900

    Create UserServiceTest_2.java

---
+++ Spring/ch_05/5.1/UserServiceTest_2.java
@@ -0,0 +1,24 @@
+//간결해진 upgradeLevel 테스트
+@Test
+	public void upgradeLevels() {
+		userDao.deleteAll();
+		for(User user : users) userDao.add(user);
+		
+		userService.upgradeLevels();
+		
+		checkLevelUpgraded(users.get(0), false);
+		checkLevelUpgraded(users.get(1), true);
+		checkLevelUpgraded(users.get(2), false);
+		checkLevelUpgraded(users.get(3), true);
+		checkLevelUpgraded(users.get(4), false);
+	}
+
+	private void checkLevelUpgraded(User user, boolean upgraded) {
+		User userUpdate = userDao.get(user.getId());
+		if (upgraded) {
+			assertThat(userUpdate.getLevel(), is(user.getLevel().nextLevel()));
+		}
+		else {
+			assertThat(userUpdate.getLevel(), is(user.getLevel()));
+		}
+	}
