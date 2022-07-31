425b2a55aa377e98871ef777c3602918fd5f8504
Park JuHo <86903267+Lokiest@users.noreply.github.com>
2022-07-31 21:45:04 +0900

    Create add()_Test.java

---
+++ Spring/ch_05/5.1/add()_Test.java
@@ -0,0 +1,19 @@
+//add 메소드의 테스트
+@Test 
+	public void add() {
+		userDao.deleteAll();
+		
+		User userWithLevel = users.get(4);	  // GOLD 레벨  
+		User userWithoutLevel = users.get(0);  //레벨 비어있는 경우
+		userWithoutLevel.setLevel(null);
+		
+		userService.add(userWithLevel);	  
+		userService.add(userWithoutLevel);
+		
+		User userWithLevelRead = userDao.get(userWithLevel.getId());
+		User userWithoutLevelRead = userDao.get(userWithoutLevel.getId());
+		//DB에 저장된 결과 가져와서 확인
+
+		assertThat(userWithLevelRead.getLevel(), is(userWithLevel.getLevel())); 
+		assertThat(userWithoutLevelRead.getLevel(), is(Level.BASIC));
+	}
