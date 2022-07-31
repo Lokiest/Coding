58fe002ad2e3eadaecb3828639eb19b00bf9bd45
Park JuHo <86903267+Lokiest@users.noreply.github.com>
2022-07-31 21:48:20 +0900

    Create UserService_2.java

---
+++ Spring/ch_05/5.1/UserService_2.java
@@ -0,0 +1,14 @@
+//정수형 상수 도입
+public class UserService {
+	public static final int MIN_LOGCOUNT_FOR_SILVER = 50;
+	public static final int MIN_RECCOMEND_FOR_GOLD = 30;
+
+	private boolean canUpgradeLevel(User user) {
+		Level currentLevel = user.getLevel(); 
+		switch(currentLevel) {                                   
+		case BASIC: return (user.getLogin() >= MIN_LOGCOUNT_FOR_SILVER); 
+		case SILVER: return (user.getRecommend() >= MIN_RECCOMEND_FOR_GOLD);
+		case GOLD: return false;
+		default: throw new IllegalArgumentException("Unknown Level: " + currentLevel); 
+		}
+	}
