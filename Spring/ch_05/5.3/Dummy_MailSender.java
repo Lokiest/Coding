16499f39f67ce7d6525816569621e6f4117fece2
Park JuHo <86903267+Lokiest@users.noreply.github.com>
2022-08-15 13:47:20 +0900

    Create Dummy_MailSender.java

---
+++ Spring/ch_05/5.3/Dummy_MailSender.java
@@ -0,0 +1,14 @@
+//더미 MailSender 구현 클래스
+package springbook.user.service;
+
+import org.springframework.mail.MailException;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
+
+public class DummyMailSender implements MailSender {
+	public void send(SimpleMailMessage mailMessage) throws MailException {
+	}
+
+	public void send(SimpleMailMessage[] mailMessage) throws MailException {
+	}
+}
