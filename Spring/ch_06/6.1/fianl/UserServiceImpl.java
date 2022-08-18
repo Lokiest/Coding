1ce3362083a6d5c514a465725fcc303d3fe5c6bc
Park JuHo <86903267+Lokiest@users.noreply.github.com>
2022-08-18 16:52:43 +0900

    Create UserServiceImpl.java

---
+++ Spring/ch_06/6.1/fianl/UserServiceImpl.java
@@ -0,0 +1,66 @@
+package springbook.user.service;
+
+import java.util.List;
+
+import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
+
+import springbook.user.dao.UserDao;
+import springbook.user.domain.Level;
+import springbook.user.domain.User;
+
+public class UserServiceImpl implements UserService {
+	public static final int MIN_LOGCOUNT_FOR_SILVER = 50;
+	public static final int MIN_RECCOMEND_FOR_GOLD = 30;
+
+	private UserDao userDao;
+	private MailSender mailSender;
+
+	public void setUserDao(UserDao userDao) {
+		this.userDao = userDao;
+	}
+
+	public void setMailSender(MailSender mailSender) {
+		this.mailSender = mailSender;
+	}
+	
+	public void upgradeLevels() {
+		List<User> users = userDao.getAll();
+		for (User user : users) {
+			if (canUpgradeLevel(user)) {
+				upgradeLevel(user);
+			}
+		}
+	}
+	
+	private boolean canUpgradeLevel(User user) {
+		Level currentLevel = user.getLevel(); 
+		switch(currentLevel) {                                   
+		case BASIC: return (user.getLogin() >= MIN_LOGCOUNT_FOR_SILVER); 
+		case SILVER: return (user.getRecommend() >= MIN_RECCOMEND_FOR_GOLD);
+		case GOLD: return false;
+		default: throw new IllegalArgumentException("Unknown Level: " + currentLevel); 
+		}
+	}
+
+	protected void upgradeLevel(User user) {
+		user.upgradeLevel();
+		userDao.update(user);
+		sendUpgradeEMail(user);
+	}
+	
+	private void sendUpgradeEMail(User user) {
+		SimpleMailMessage mailMessage = new SimpleMailMessage();
+		mailMessage.setTo(user.getEmail());
+		mailMessage.setFrom("useradmin@ksug.org");
+		mailMessage.setSubject("Upgrade ¾È³»");
+		mailMessage.setText("»ç¿ëÀÚ´ÔÀÇ µî±ÞÀÌ " + user.getLevel().name());
+		
+		this.mailSender.send(mailMessage);
+	}
+	
+	public void add(User user) {
+		if (user.getLevel() == null) user.setLevel(Level.BASIC);
+		userDao.add(user);
+	}
+}
