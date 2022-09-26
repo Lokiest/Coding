88c871716a2b100ab3ef5ff47143e7a7f6768ccf
Park JuHo <86903267+Lokiest@users.noreply.github.com>
2022-09-26 20:25:01 +0900

    Create Helloworld.java

---
+++ BackEnd_Program/Programming_for_NaturalLanguageProcessing/Day_6/src/day01/Helloworld.java
@@ -0,0 +1,11 @@
+package day01;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("dfewr");
+		System.out.println('B'); //홑따옴표는 문자 하나만
+		
+	}
+}
