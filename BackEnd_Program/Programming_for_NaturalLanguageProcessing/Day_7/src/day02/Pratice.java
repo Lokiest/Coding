bbf988e2ef22b786f4554bbbec6b7e5bbf3bc43e
Park JuHo <86903267+Lokiest@users.noreply.github.com>
2022-09-28 09:07:22 +0900

    Create Pratice.java

---
+++ BackEnd_Program/Programming_for_NaturalLanguageProcessing/Day_7/src/day02/Pratice.java
@@ -0,0 +1,28 @@
+package day02;
+
+public class Practice {
+
+	public static void main(String[] args) {
+		//[2]
+		long regNo = ' ';
+		//[3]
+		//literal : 100, 100L, 3.14f
+		//variable : i, l
+		//keyword : int, long, final, float
+		//상수 : (final로 표기된) Pi (=const)
+		//[4]
+		//Byte >> 클래스유형으로 기본자료형이 아님
+		//[5]
+		//12, true, 131(char+char=정수), 51(char+int), 99, Java, error
+		//[6]
+		//True, NULL, Class, System
+		//[7]
+		//$ystem, #불가능, 숫자시작 불가능, If, 자바, 키워드불가능, $llk_ljl, @불가능 
+		//[8]
+		//int, float
+		//[9]
+		//float f = (float)l; >> 형변환 생략가능, i = (int)ch; 가
+
+	}
+
+}
