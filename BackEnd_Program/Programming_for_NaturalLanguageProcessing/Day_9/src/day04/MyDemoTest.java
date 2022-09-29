84220fadff501604567c26c0677933e15aff8e6e
Park JuHo <86903267+Lokiest@users.noreply.github.com>
2022-09-29 17:47:37 +0900

    Update MyDemoTest.java

--- BackEnd_Program/Programming_for_NaturalLanguageProcessing/Day_9/src/day04/MyDemoTest.java
+++ BackEnd_Program/Programming_for_NaturalLanguageProcessing/Day_9/src/day04/MyDemoTest.java
@@ -1,36 +1,34 @@
 package day04;
 
-public class YDemo {
-	String str = "YDemo's Variable";
-	float pi; //생성자 구성하고 초기값 부여
-	static int add;
-	static {
-		add = 3;
-		//static 블럭은 main메서드보다 먼저 메모리에 올라감
-	}
-	public YDemo() {
-		pi = 3.14f;
-	}
-	public void foo() {
-		System.out.println("foo-------------");
-		System.out.print(pi);
-	}
-	public static void bar() {
-		System.out.println("bar-------------");
-		System.out.println(new YDemo().pi);
-	}
-	
+public class MyDemoTest {
+
 	public static void main(String[] args) {
-		System.out.println("main() method----------------");
-		System.out.println(add);
-		YDemo y = new YDemo();
-//		System.out.println(pi); [x]
-		System.out.println(y.pi);
+		MyDemo md = new MyDemo();
 		
-		//bar call
-		YDemo.bar();
-		//foo call
-		new YDemo().foo();
-		y.foo();
+		MyDemo md2 = new MyDemo();
+		MyDemo md3 = md2;
+		System.out.println(md);
+		System.out.println(md2);
+		
+		System.out.println(md2==md);
+		System.out.println(md2==md3);
+		
+		System.out.println(md.x);
+		System.out.println(md2.x);
+		System.out.println(md3.x);
+		
+		md2.x = 500;
+		System.out.println(md.x);
+		System.out.println(md2.x);
+		System.out.println(md3.x);
+		
+		System.out.println(MyDemo.y);		
+		
+		YDemo yd = new YDemo();
+		System.out.println(yd.str);
+		System.out.println(yd.pi);
+		System.out.println(YDemo.add);
+		
 	}
+
 }
