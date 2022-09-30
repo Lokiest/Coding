4713c4ec3a06f88e0709389fef2c4a22f9fef1e8
Park JuHo <86903267+Lokiest@users.noreply.github.com>
2022-09-30 20:42:53 +0900

    Create Yuja.java

---
+++ BackEnd_Program/Programming_for_NaturalLanguageProcessing/Day_9/Day_9/scr/day05/Yuja.java
@@ -0,0 +1,22 @@
+package day05;
+//유자 + 설탕
+public class Yuja {
+	private int yuja;
+	private int sugar;
+	
+	//setter, getter
+	public int getYuja() {
+		return yuja;
+	}
+	public void setYuja(int yuja) {
+		this.yuja = yuja;
+	}
+	public int getSugar() {
+		return sugar;
+	}
+	public void setSugar(int sugar) {
+		this.sugar = sugar;
+	}
+	
+	
+}
