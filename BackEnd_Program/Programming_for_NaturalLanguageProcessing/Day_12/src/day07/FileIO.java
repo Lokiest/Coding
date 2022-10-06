30f14667ef6a171ca71e416654f9b4e9d6827eb8
Park JuHo <86903267+Lokiest@users.noreply.github.com>
2022-10-06 09:03:35 +0900

    Update FileIO.java

--- BackEnd_Program/Programming_for_NaturalLanguageProcessing/Day_12/src/day07/FileIO.java
+++ BackEnd_Program/Programming_for_NaturalLanguageProcessing/Day_12/src/day07/FileIO.java
@@ -1,40 +1,46 @@
 package day07;
 
+import java.io.*;
 import javax.swing.*;
-import java.awt.*;
-import java.awt.event.*;
+//파일을 읽어서 콘솔에 출력하는 프로그램 구현
+//[1] try catch
+//[2] throws
 
-public class PongGui extends JFrame {
+public class FileIO {
 	
-	Container cp;
-	JPanel p1, p2;
-	CardLayout card;
-	
-	public PongGui() {
-		cp = this.getContentPane();
-		cp.setLayout(card = new CardLayout()); //카드 레이아웃 설정
-		p1 = new MyLoginPanel();
-		p2 = new JPanel();
+	public static String reading(String fileName) {
+		System.out.println("-----" + fileName + "-----");
+		String contents = "";
+		FileReader fr = null;
+		char[] data = new char[1000];
+		try {
+			fr = new FileReader(fileName);
+			//파일과 노드 연결
+			
+			fr.read(data, 0, 1000);
+			//파일 읽어서 그 내용을 data배열에 담음
+			
+			fr.close();
+			//노드 연결 종료 (파일 리소스 반환)
+		} catch (FileNotFoundException e) {
+			System.out.println("파일 찾을 수 없음");
+			e.printStackTrace();
+		} catch (IOException e) {
+			System.out.println("파일 읽을 수 없음");
+		}
 		
-		cp.add(p1, "Log in");
-		cp.add(p2, "Home");
+		contents = new String(data);
 		
-		p1.setBackground(Color.yellow);
-		p2.setBackground(Color.pink);
-		
-//		card.show(cp, "Home");
-//		p1.btnLogin.addActionListener(this);
-		
-		this.setSize(400, 600);
-		this.setLocation(500, 500);
-		this.setVisible(true);
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
+		return contents;
 	}
 
 	public static void main(String[] args) {
-		new PongGui();
 
+		String fname = JOptionPane.showInputDialog("파일명 입력 : ");
+		if (fname == null) return;
+		String str = reading(fname);
+		System.out.println(str);
+		
 	}
 
 }
