b20c388e45975de0adbade33c3fcfd6bdb6b4555
Park JuHo <86903267+Lokiest@users.noreply.github.com>
2022-10-12 16:43:55 +0900

    Create NotSupportedNameException.java

---
+++ BackEnd_Program/Programming_for_NaturalLanguageProcessing/Day_15/src/my/app2/NotSupportedNameException.java
@@ -0,0 +1,13 @@
+package my.app2;
+//1. Exception을 상속받는다
+//2. 생성자를 만들고 super("예외메시지");를 호출한다
+public class NotSupportedNameException extends Exception
+{
+	public NotSupportedNameException() {
+		super("NotSupportedNameException");
+	}
+	public NotSupportedNameException(String msg) {
+		super(msg);
+	}
+
+}/////////////////////////////////////////////
