7b8c27499050ff157a572f839d46302017a436ed
Park JuHo <86903267+Lokiest@users.noreply.github.com>
2022-11-03 00:23:25 +0900

    Create DBUtil.java

---
+++ BackEnd_Program/Data_Saving&Mining/MemoApp/src/com.multicamp/DBUtil.java
@@ -0,0 +1,25 @@
+package com.multicamp;
+
+import java.sql.*;
+
+public class DBUtil {
+	
+	static String url = "jdbc:oracle:thin:@localhost:1521:XE";
+	static String user = "scott", pwd = "tiger";
+	
+	static {
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+		} catch(ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public static Connection getCon() throws SQLException {
+		Connection con = DriverManager.getConnection(url, user, pwd);
+		return con;
+	}
+	
+
+}
